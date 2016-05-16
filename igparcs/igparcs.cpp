e7d8caea47afb3b4e50b91444a4802baef19e9fc
mori-inj <leokim1022@snu.ac.kr>
2016-05-16 15:07:38 +0900

    baisc collision doen

--- igparcs/igparcs.cpp
+++ igparcs/igparcs.cpp
@@ -1,9 +1,11 @@
 #include <windows.h>
 #include <gdiplus.h>
+#include <time.h>
 //#include "resource.h"
 using namespace Gdiplus;
 #pragma comment(lib, "gdiplus")
 
+#define LD long double
 
 
 HINSTANCE g_hInst;
@@ -13,6 +15,97 @@ LPCTSTR lpszClass = TEXT("GdiPlusStart");
 void OnPaint(HDC hdc, int ID, int x, int y);
 void OnPaintA(HDC hdc, int ID, int x, int y, double alpha);
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+
+class Particle
+{
+	private:
+		int size;
+		HPEN hPen;
+	public:
+		COLORREF color;
+		int x,y;
+		LD vx, vy;
+		Particle()
+		{
+			this->x = rand()%80*10;
+			this->y = rand()%600-60;
+			vx = rand()%6-3;
+			vy = 0;//rand()%2-1;
+			while(vx==0 && vy ==0)
+			{
+				vx = rand()%6-3;
+				vy = 0;//rand()%2-1;
+			}
+			size = 3;
+			color = RGB(rand()%256, rand()%256, rand()%256);
+			hPen = CreatePen(PS_SOLID, 1, color);
+		}
+		Particle(LD x, LD y)
+		{
+			this->x = x;
+			this->y = y;
+			vx = rand()%2-1;
+			vy = rand()%2-1;
+			while(vx==0 && vy ==0)
+			{
+				vx = rand()%2-1;
+				vy = rand()%2-1;
+			}
+			size = 3;
+			color = RGB(rand()%256, rand()%256, rand()%256);
+			hPen = CreatePen(PS_SOLID, 1, color);
+		}
+
+		void draw(HDC hdc)
+		{
+			SelectObject(hdc, hPen);
+			Ellipse(hdc, x-size, y-size, x+size, y+size);
+		}
+		void collision(Particle that)
+		{
+			if( (this->x - that.x)*(this->x - that.x) + (this->y - that.y)*(this->y - that.y) <= 64)
+			{
+				vx = -vx;
+				vy = -vy;
+				that.vx = -that.vx;
+				that.vy = -that.vy;
+
+				x = x + vx;
+				y = y + vy;
+				that.x = that.x + that.vx;
+				that.y = that.y + that.vy;
+
+			}
+		}
+		void move()
+		{
+			x = x + vx;
+			y = y + vy;
+			if(y<0)
+			{
+				y=0;
+				vy = -vy;
+			}
+			else if(y>600-60)
+			{
+				y = 600-60;
+				vy = -vy;
+			}
+
+			if(x<0)
+			{
+				x=0;
+				vx = -vx;
+			}
+			else if(x>800)
+			{
+				x = 800;
+				vx = -vx;
+			}
+		}
+		//??
+};
+
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
 {
@@ -70,13 +163,21 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 	HDC hdc, MemDC;
 	PAINTSTRUCT ps;
 
+	HBRUSH hBrush;
+	HBRUSH oldBrush;
+	HPEN hPen;
+	HPEN oldPen;
+
 	HBITMAP hBit, OldBit;
 	RECT crt;
+
+	static Particle particle[50];
 
 
 	switch (iMsg)
 	{
 	case WM_CREATE:
+		srand((unsigned)time(NULL));
 		SetTimer(hWnd, 1, 10, 0);
 		break;
 
@@ -90,14 +191,23 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		MemDC = CreateCompatibleDC(hdc);
 		hBit = CreateCompatibleBitmap(hdc, crt.right, crt.bottom);
 		OldBit = (HBITMAP)SelectObject(MemDC, hBit);
-		//hBrush = CreateSolidBrush(RGB(255, 255, 255));
-		//oldBrush = (HBRUSH)SelectObject(MemDC, hBrush);
-		//hPen = CreatePen(PS_SOLID, 5, RGB(255, 255, 255));
-		//oldPen = (HPEN)SelectObject(MemDC, hPen);
+		hBrush = CreateSolidBrush(RGB(0, 0, 0));
+		oldBrush = (HBRUSH)SelectObject(MemDC, hBrush);
+		hPen = CreatePen(PS_SOLID, 1, RGB(255, 255, 255));
+		oldPen = (HPEN)SelectObject(MemDC, hPen);
 
 		//FillRect(MemDC, &crt, hBrush);
 		SetBkColor(MemDC, RGB(255, 255, 255));
 
+		for(int i=0; i<50; i++)
+			for(int j=i+1; j<50; j++)
+				particle[i].collision(particle[j]);
+
+		for(int i=0; i<50; i++)
+		{
+			particle[i].move();
+			particle[i].draw(MemDC);
+		}
 
 
 		//OnPaint(MemDC, TITLE0, 0, 0);
@@ -105,10 +215,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		BitBlt(hdc, 0, 0, crt.right, crt.bottom, MemDC, 0, 0, SRCCOPY);
 		SelectObject(MemDC, OldBit);
 		DeleteDC(MemDC);
-		//SelectObject(MemDC, oldPen);
-		//DeleteObject(hPen);
-		//SelectObject(MemDC, oldBrush);
-		//DeleteObject(hBrush);
+		SelectObject(MemDC, oldPen);
+		DeleteObject(hPen);
+		SelectObject(MemDC, oldBrush);
+		DeleteObject(hBrush);
 		DeleteObject(hBit);
 
 		EndPaint(hWnd, &ps);
